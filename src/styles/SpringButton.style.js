0e3609f3ebd9e497fc62fde951e911f814b3bb25
Suraj Behera <call2suraj@gmail.com>
2022-12-14 10:28:45 +0530

    code refactor

---
+++ src/styles/SpringButton.style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        padding: 30
+    },
+    header: {
+        backgroundColor: '#0c570a', 
+        height: 30, 
+        marginTop: 3, 
+        marginBottom: 5
+    },
+    text_style: {
+        fontSize: 18, 
+        color:'white', 
+        textAlign:'center'
+    }
+});
